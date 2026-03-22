32c640412339c7a0d2bbe4a3ba02f062e55162b3
thejunghare <prasadjunghare2@gmail.com>
2026-03-22 20:10:03 +0530

    updated again

---
+++ destructuring.js
@@ -0,0 +1,5 @@
+const person = {
+  name: "John",
+  age: 30,
+  city: "New York",
+};
---
+++ ternaryOperator.js
@@ -0,0 +1,32 @@
+const exerciseTimeMins = 20;
+let message = "";
+
+/*
+if (exerciseTimeMins < 30) {
+  message = "You need to work hard";
+} else {
+  message = "Doing good";
+}
+
+if (exerciseTimeMins < 30) {
+  message = "You need to work hard";
+} else if (exerciseTimeMins < 60) {
+} else {
+  message = "Doing good";
+}
+*/
+
+// challenge : refactor the if else statement to use a ternary operator
+/*
+const playerGuess = 3;
+const correctAnswer = 6;
+
+let message = "";
+if (playerGuess === correctAnswer) {
+  message = "correct";
+} else {
+  message = "wrong";
+}
+
+console.log(message);
+ */
